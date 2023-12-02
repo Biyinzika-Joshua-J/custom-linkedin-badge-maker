77458daeae717f622d7d4de1545f00a85b73d108
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-12-02 15:12:58 +0300

    setting up croppper js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,39 +1,40 @@
-import Image from 'next/image'
-import MainCard from '@/components/cards/MainCard'
-import { ExamplesOfCustomBadges } from '@/constants/constants'
-import FileUpload from '@/components/upload/FileUpload/FileUpload'
-import Cropper from '@/components/cropper/Cropper'
+import Image from "next/image";
+import MainCard from "@/components/cards/MainCard";
+import { ExamplesOfCustomBadges } from "@/constants/constants";
+import FileUpload from "@/components/upload/FileUpload/FileUpload";
+import Cropper from "@/components/cropper/Cropper";
 
 export default function Home() {
   return (
     <main className="flex-1">
-       <div className="mx-auto lg:w-[80%] my-20">
-       <div className="">
-         {/* <FileUpload/> */}
-          <Cropper/>
+      <div className="mx-auto lg:w-[80%] my-20">
+        <div className="">
+          {/* <FileUpload/> */}
+          <div className="">
+            <h2 className="text-center text-3xl font-bold pb-4">Crop your Image</h2>
+            <div className="h-96 w-96 relative mx-auto">
+              <Cropper />
+            </div>
+          </div>
         </div>
 
         {/*TODO: When a user is logined - show there previous profile pics instead*/}
         <div className="py-20">
-          <h2 className="text-center text-3xl font-bold">
-            Some Examples
-          </h2>
+          <h2 className="text-center text-3xl font-bold">Some Examples</h2>
           <div className="mt-10 py-10 flex justify-center bg-gray-100">
-          <div className=" grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5">
-            {
-              ExamplesOfCustomBadges.map((item, idx) => (
-                <MainCard 
+            <div className=" grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5">
+              {ExamplesOfCustomBadges.map((item, idx) => (
+                <MainCard
                   imageUrl={item.image}
                   badgeColor={item.badgeColor}
                   badgeLabel={item.badgeLabel}
                   key={idx}
                 />
-              ))
-            }
-          </div>
+              ))}
+            </div>
           </div>
         </div>
-       </div>
+      </div>
     </main>
-  )
+  );
 }
--- components/cropper/Cropper.tsx
+++ components/cropper/Cropper.tsx
@@ -1,26 +1,68 @@
 "use client"
-import React, { useRef } from "react";
-import Cropper, { ReactCropperElement } from "react-cropper";
-import "cropperjs/dist/cropper.css";
+import { useState, useCallback } from 'react'
+import Cropper from 'react-easy-crop'
 
-const ImageCropper = () => {
-  const cropperRef = useRef<ReactCropperElement>(null);
-  const onCrop = () => {
-    const cropper = cropperRef.current?.cropper;
-    console.log(cropper.getCroppedCanvas().toDataURL());
+
+const CROP_AREA_ASPECT = 3 / 2;
+
+const Output = ({ croppedArea }) => {
+  const scale = 100 / croppedArea.width;
+  const transform = {
+    x: `${-croppedArea.x * scale}%`,
+    y: `${-croppedArea.y * scale}%`,
+    scale,
+    width: "calc(100% + 0.5px)",
+    height: "auto"
+  };
+
+  const imageStyle = {
+    transform: `translate3d(${transform.x}, ${transform.y}, 0) scale3d(${transform.scale},${transform.scale},1)`,
+    width: transform.width,
+    height: transform.height
   };
 
   return (
-    <Cropper
-      src="https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg"
-      style={{ height: 400, width: "100%" }}
-      // Cropper.js options
-      initialAspectRatio={16 / 9}
-      guides={false}
-      crop={onCrop}
-      ref={cropperRef}
-    />
+    <div
+      className="output"
+      style={{ paddingBottom: `${100 / CROP_AREA_ASPECT}%` }}
+    >
+      <img src="/assets/images/example.jpg" alt="" style={imageStyle} />
+    </div>
   );
 };
 
+
+
+const ImageCropper = () => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+  const [croppedArea, setCroppedArea] = useState(null);
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels)
+  }, [])
+
+  return (
+    <div className="">
+      <Cropper
+        image={"/assets/images/example.jpg"}
+        crop={crop}
+        zoom={zoom}
+        aspect={16 / 9}
+        onCropChange={setCrop}
+        onCropComplete={onCropComplete}
+        onZoomChange={setZoom}
+        cropSize={{width:200, height:200}}
+      />
+       <div className="viewer">
+        {/*<div>{crop && <Output croppedArea={crop} />}</div>*/}
+      </div>
+    </div>
+  )
+}
+
 export default ImageCropper;
+
+
+
+
